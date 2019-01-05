868976a2a80c3300c6164dc41ce06917697ece3d
Nafis Sajid <nafissajidns@gmail.com>
2019-01-05 17:08:12 +0600

    Initial Commit

--- app/src/main/java/com/example/nafissajid/basicknowledge/SignUpActivity.kt
+++ app/src/main/java/com/example/nafissajid/basicknowledge/SignUpActivity.kt
@@ -2,11 +2,16 @@ package com.example.nafissajid.basicknowledge
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUpActivity : AppCompatActivity() {
 
+    private var mAuth: FirebaseAuth? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        mAuth = FirebaseAuth.getInstance()
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
         sign_up_button.setOnClickListener{
