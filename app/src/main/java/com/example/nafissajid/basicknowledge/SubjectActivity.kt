63b90b6b2be891e9ed811ee33e80bf82f11b94da
Nafis Sajid <nafissajidns@gmail.com>
2019-01-17 21:10:47 +0600

    Update

--- app/src/main/java/com/example/nafissajid/basicknowledge/SubjectActivity.kt
+++ app/src/main/java/com/example/nafissajid/basicknowledge/SubjectActivity.kt
@@ -3,17 +3,19 @@ package com.example.nafissajid.basicknowledge
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kotlinx.android.synthetic.main.activity_subject.*
 
-class SubjectActivity : AppCompatActivity(){
-
+class SubjectActivity : AppCompatActivity(), View.OnClickListener{
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_subject)
-        buttonp1.setOnClickListener {
-            val intent = Intent(this, LessonActivity::class.java)
-            startActivity(intent)
-        }
+        buttonp1.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        val intent = Intent(this, LessonActivity::class.java)
+        startActivity(intent)
     }
 }
