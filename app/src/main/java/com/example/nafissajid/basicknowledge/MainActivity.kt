6eeb945ce0921f93d1fd2aeb277c3814e4606065
Nafis Sajid <nafissajidns@gmail.com>
2019-01-17 23:17:43 +0600

    Update

--- app/src/main/java/com/example/nafissajid/basicknowledge/MainActivity.kt
+++ app/src/main/java/com/example/nafissajid/basicknowledge/MainActivity.kt
@@ -1,10 +1,11 @@
 package com.example.nafissajid.basicknowledge
 
+
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.PopupWindow
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -25,10 +26,24 @@ class MainActivity : AppCompatActivity() {
         }
 
         about_button.setOnClickListener {
+            val dialogBuilder = AlertDialog.Builder(this)
+            dialogBuilder.setTitle("About Us")
+            dialogBuilder.setMessage(R.string.about_us)
+            dialogBuilder.setPositiveButton("OK"){dialog, which ->  
+                
+            }
+            dialogBuilder.create().show()
             Toast.makeText(this, "About Clicked", Toast.LENGTH_SHORT).show()
         }
 
         rate_button.setOnClickListener {
+            val dialogBuilder = AlertDialog.Builder(this)
+            dialogBuilder.setTitle("Rate Us")
+            dialogBuilder.setMessage(R.string.rate_us)
+            dialogBuilder.setPositiveButton("OK"){dialog, which ->
+
+            }
+            dialogBuilder.create().show()
             Toast.makeText(this, "Rate us Clicked", Toast.LENGTH_SHORT).show()
         }
     }
